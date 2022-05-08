9fdd83fdd1897ba81837c3899f1503fbd79e3ea0
len.choi <len.choi@cyworldlabs.com>
2022-05-08 19:59:56 +0900

    first commit

---
+++ src/main/kotlin/com/wedo/mwohalkka/MwohalkkaApplication.kt
@@ -0,0 +1,11 @@
+package com.wedo.mwohalkka
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MwohalkkaApplication
+
+fun main(args: Array<String>) {
+    runApplication<MwohalkkaApplication>(*args)
+}
---
+++ src/test/kotlin/com/wedo/mwohalkka/MwohalkkaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wedo.mwohalkka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MwohalkkaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
